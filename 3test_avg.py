bde0a24fa232a00c1d5a86cd41baf871857e9b4d
Srivatsa j bhat <94160935+Srivatsajbhat@users.noreply.github.com>
2023-07-20 14:57:37 +0530

    Update 3test_avg.py

--- 3test_avg.py
+++ 3test_avg.py
@@ -1,34 +1,16 @@
-def is_numeric(input_str):
-    return input_str.replace(".", "", 1).isdigit()
+first_test_m1 = int (input("Enter the marks in the first test: "))
+second_test_m2 = int (input("Enter the marks in second test: "))
+third_test_m3 = int (input("Enter the marks in third test: "))
 
-
-def calculate_average(test1, test2, test3):
-    # Find the minimum mark and exclude it from the calculation
-    min_mark = min(test1, test2, test3)
+if (first_test_m1 > second_test_m2):
+    if (second_test_m2 > third_test_m3):
+        total = first_test_m1 + second_test_m2
+    else: 
+        total = first_test_m1 + third_test_m3
+elif (first_test_m1 > third_test_m3):
+    total = first_test_m1 + third_test_m2
+else:
+    total = second_test_m2 + third_test_m3
     
-    # Calculate the average of the best two marks
-    best_average = (test1 + test2 + test3 - min_mark) / 2
-    
-    return best_average
-
-
-def get_test_marks():
-    while True:
-        test1 = input("Enter the first test mark: ")
-        test2 = input("Enter the second test mark: ")
-        test3 = input("Enter the third test mark: ")
-        
-        if is_numeric(test1) and is_numeric(test2) and is_numeric(test3):
-            return float(test1), float(test2), float(test3)
-        else:
-            print("Invalid input. Please enter valid numeric test marks.")
-
-
-def main():
-    test1, test2, test3 = get_test_marks()
-    best_average = calculate_average(test1, test2, test3)
-    print(f"The best average of the two tests is: {best_average:.2f}")
-
-
-if __name__ == "__main__":
-    main()
+Avg = total / 2
+print ("The average of the best two test marks is: ",Avg)
